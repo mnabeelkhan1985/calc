f072e35e6141253d7a6e6b1d31b60941bbf7aca8
mnabeelkhan1985 <mnabeelkhan1985@gmail.com>
2021-06-15 02:29:01 +0500

    calculator

---
+++ app.js
@@ -0,0 +1,30 @@
+var input = document.getElementById('display')
+
+function val(num) {
+
+    if (input.value.match(/[-,+,*,/,%]/) && num.match(/[-,+,*,/,%]/)) {
+
+        input.value = input.value.slice(0,input.value.length -1) + num;
+        
+    }
+
+    else { input.value += num }
+
+
+}
+
+function calc() {
+    if (input.value === "") {
+        input.value = ""
+    } else {
+        input.value = eval(input.value)
+    }
+}
+
+function clr() {
+    input.value = ''
+}
+
+function back() {
+    input.value = input.value.slice(0, input.value.length - 1)
+}
